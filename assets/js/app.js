939e01061c7c7d977d1d8fce5d2ff296b7b90b24
Pina Vetrano <pinavetranopro@gmail.com>
2023-02-23 15:13:27 +0100

    add fonts doc and input on css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,29 @@
-'use strict';
-// image animation (joao)
+'use strict'; 
 
-var upItem = document.querySelector('.images--right');
-var bottomItem = document.querySelector('.images--left');
-const sideItem = document.querySelector('.svg__side');
+// pour prendre la classe du bouton 
 
-window.addEventListener("scroll", function() {
-    upItem.style.transform = "translateX(" + window.pageYOffset / 15 + "px)";
-    bottomItem.style.transform = "translateX(" + -window.pageYOffset / 10 + "px)";
-    sideItem.style.transform = "translateY(" + window.pageYOffset / 5 + "px)";
-});
+const themeTrigger = document.querySelector(".js-theme-trigger");
+if(themeTrigger){
+    themeTrigger.addEventListener("click", toggleTheme); //quand on inclu un click sur le bouton, on met un toggleTheme
+}
+
+function toggleTheme(){ // pour changer le data attribut on doit d'abord récupérer l'attribut sur le body qui est data-theme.
+    let theme = document.body.getAttribute("data-theme");
+    if(theme == "dark"){
+        setTheme("light");
+    }else{
+        setTheme("dark");
+    }
+}
+
+
+function setTheme(theme){
+    document.body.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+}
+
+let activeTheme = localStorage.getItem("theme");
+if(activeTheme !== null){
+    setTheme(activeTheme);
+}
+// permet de prendre en compte la dernière modification du theme.
