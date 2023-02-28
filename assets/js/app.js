573cb54ee025b96a3d5fd2defb63953aa5267192
Pina Vetrano <pinavetranopro@gmail.com>
2023-02-28 09:46:00 +0100

    index page at user Journey + js close nav

--- assets/js/app.js
+++ assets/js/app.js
@@ -44,9 +44,47 @@ function toggleNavigation() {
  }
 }
 
+//BURGER MENU // FERMER NAV QUAND ON CLIQUE SUR UN LIEN // 
+
+let closeMenu = document.querySelector(".list__m--header");
+
+if(closeMenu){
+
+closeMenu.addEventListener("click", MenuList);
+
+
+    function MenuList (){
+        if(!document.body.hasAttribute("data-menu")){
+            document.body.setAttribute("data-menu", false);
+        } else {
+            document.body.removeAttribute("data-menu");
+        }
+    }
+
+}
 
 // date copyright
 let answerDate = document.querySelector(".date");
 let today = new Date();
 let year = today.getFullYear();
 answerDate.innerHTML = year;
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
