4869e95e9345a68b91e7a1e49b4e0f08770ee43b
Pina Vetrano <pinavetranopro@gmail.com>
2023-03-29 16:51:17 +0200

    no more issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,25 +55,6 @@ let today = new Date();
 let year = today.getFullYear();
 answerDate.innerHTML = year;
 
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
 
 var xValues = ["Restaurer", "Verdure", "Détente", "Sociabiliser","Couvert"];
 var yValues = [18, 25, 20, 11, 17];
