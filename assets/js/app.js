3cb35c3b24d11bedcc817161ccb32020bf2011a3
Pina Vetrano <pinavetranopro@gmail.com>
2023-03-30 22:37:24 +0200

    add all

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,37 @@
 'use strict'; 
 
+
 // pour prendre la classe du bouton 
 
-const themeTrigger = document.querySelector(".js-theme-trigger");
-if(themeTrigger){
-    themeTrigger.addEventListener("click", toggleTheme); //quand on inclu un click sur le bouton, on met un toggleTheme
-}
 
-function toggleTheme(){ // pour changer le data attribut on doit d'abord récupérer l'attribut sur le body qui est data-theme.
-    let theme = document.body.getAttribute("data-theme");
-    if(theme == "dark"){
-        setTheme("light");
-    }else{
-        setTheme("dark");
-    }
-}
-
-
-function setTheme(theme){
-    document.body.setAttribute("data-theme", theme);
-    localStorage.setItem("theme", theme);
-}
-
+let darkmodeButtonEl = document.querySelector(".js-theme-trigger")
 let activeTheme = localStorage.getItem("theme");
-if(activeTheme !== null){
-    setTheme(activeTheme);
+ 
+if (activeTheme) {
+ setTheme(activeTheme);
+ if(activeTheme == "dark"){
+ }
 }
-// permet de prendre en compte la dernière modification du theme.
-
-
+ 
+if (darkmodeButtonEl) {
+ darkmodeButtonEl.addEventListener("click", toggleTheme);
+}
+ 
+function toggleTheme() {
+ let theme = document.body.getAttribute("data-theme");
+ if (theme == "dark") {
+ document.body.setAttribute('data-theme', theme);
+ setTheme("light");
+ 
+ } else {
+ setTheme("dark")
+ }
+}
+ 
+function setTheme(theme) {
+ document.body.setAttribute('data-theme', theme);
+ localStorage.setItem("theme", theme);
+}
 
 //BURGER MENU // FERMER NAV QUAND ON CLIQUE SUR UN LIEN // 
 
